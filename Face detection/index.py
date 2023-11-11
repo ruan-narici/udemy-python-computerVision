afc552812c07ff32d17848445937916a2879f610
Ruan Narici <ruannarici@hotmail.com>
2023-11-11 15:54:24 -0300

    Face detection with haarcascade and OpenCV

--- Face detection/index.py
+++ Face detection/index.py
@@ -1,16 +1,42 @@
 import cv2 as cv #Open CV
 
 image = cv.imread('./assets/Images/people1.jpg');
-print(image.shape); # Lagura, altura e Canais (RGB)
+
+# Lagura, altura e Canais (RGB)
+print(image.shape); 
 
 # cv.imshow('image', image);
 # cv.waitKey(0);
 
-image = cv.resize(image, (800, 600)); # Diminuindo o tamanho da altura e lagura.
+# Diminuindo o tamanho da altura e lagura.
+image = cv.resize(image, (800, 600)); 
 
-print(image.shape); # Lagura, altura e Canais (RGB) após o resize
+# Lagura, altura e Canais (RGB) após o resize
+print(image.shape); 
 
-imageGray = cv.cvtColor(image, cv.COLOR_BGR2GRAY); # Convertendo a image para canais de cinza
+# Convertendo a image para canais de cinza
+imageGray = cv.cvtColor(image, cv.COLOR_BGR2GRAY); 
 
-cv.imshow('image cinza', imageGray); # Exibindo a image
-cv.waitKey(0); # O programa sera executado ate que uma tecla seja pressionada.
+# Exibindo a image
+# cv.imshow('image cinza', imageGray); 
+
+# O programa sera executado ate que uma tecla seja pressionada.
+# cv.waitKey(0); 
+
+# Importando o modelo xml Cascade já treinado para deteccao de faces.
+faceDetector = cv.CascadeClassifier('./assets/Cascades/haarcascade_frontalface_default.xml');
+
+# Executando a deteccao de faces através do metodo detectMultiScale
+detections = faceDetector.detectMultiScale(imageGray);
+
+# Exibindo as deteccoes
+# Padrao de exibicao (x, y, width, height)
+print(detections);
+
+# Percorrendo as deteccoes e criando retangulos na imagem original(colorida)
+for x, y, w, h in detections: 
+    # rectangle recebe como parametro a imagem, o inicio (x, y), o tamanho (x+width, y+height), a cor BGR, o tamanho do traço
+    cv.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 4);
+
+cv.imshow('Faces detectadas', image);
+cv.waitKey(0);
